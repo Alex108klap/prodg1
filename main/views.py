58fe19b549617c25d13b5f15402317ec5ac11923
Alex108klap <zaralex108@gmail.com>
2024-04-29 17:31:26 +0200

    search full

---
+++ main/views.py
@@ -0,0 +1,21 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from goods.models import Categories
+
+
+def index(request):
+
+
+    context={'title': 'Home-Главная',
+             'content': 'Магазин мебели HOME',
+             }
+    return render(request, "main/index.html", context)
+
+
+def about(request):
+    context = {'title': 'Home- О нас',
+               'content': 'О нас',
+               'text_on_page':'Текст о том какой классный этот интернет магазин'
+               }
+    return render(request, "main/about.html", context)
+
